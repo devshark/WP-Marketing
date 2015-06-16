a98f4b033a21903183ce5f6bde2a493884dbe97b
Anthony Lim <anthony.lim@transact24.com>
2015-06-16 10:10:11 +0800

    added custom function filter for wp_title in iusecard theme

--- wp-content/themes/iusecard/functions.php
+++ wp-content/themes/iusecard/functions.php
@@ -2,3 +2,11 @@
 /**
  * Empty
  */
+add_filter('wp_title','iusecard_wp_title',10,2);
+
+function iusecard_wp_title($title){
+    if(empty($title)){
+        $title = get_bloginfo('name');
+    }
+    return $title . ' | ' . get_bloginfo('description');
+}
